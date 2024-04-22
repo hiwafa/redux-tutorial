f46953c2136af89728e8dbb0aaa646254855bb75
hiwafa <mq.func@gmail.com>
2024-04-22 16:44:42 +0200

    redux implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createStore } from "redux";
+import { Provider, connect } from 'react-redux';
+
+const initialState = {
+  counter: 1
+}
+
+const myReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD": return {
+      counter: state.counter+action.payload,
+      ...state
+    }
+    break;
+
+    case "SUB": return {
+      counter: state.counter-action.payload,
+      ...state
+    }
+    default: return state;
+  }
+
+}
+
+const store = createStore(myReducer);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Provider store={store}>
+      <MyCounterWithData />
+     </Provider>
     </div>
   );
 }
+
+
+
+const mapStateToProps = (state)=> {
+  return {
+    ...state
+  }
+}
+
+const mapDispatchToProps = (dispatch)=> {
+  return {
+    add: ()=> dispatch({
+      type: "ADD",
+      payload: 1
+    }),
+    sub: ()=> dispatch({
+      type: "SUB",
+      payload: 1
+    })
+  }
+}
+
+
+const MyCounter = (props)=> {
+
+  return <div>{props.counter}</div>
+}
+
+const MyCounterWithData = connect(mapStateToProps, mapDispatchToProps)(MyCounter);
 
 export default App;
